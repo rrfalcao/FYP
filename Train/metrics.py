4f8b987ea2c0a0e19b2a9736b4cb3ee6db251bd6
rrfalcao <ridgefalcao1@yahoo.co.uk>
2025-03-06 12:10:34 +0000

    Train 3

--- Train/metrics.py
+++ Train/metrics.py
@@ -20,3 +20,10 @@ def iou(y_true, y_pred, threshold=0.5):
     intersection = tf.reduce_sum(y_true * y_pred)                                                     
     union = tf.reduce_sum(tf.cast(y_true + y_pred > 0, tf.float32))                                 
     return intersection / union
+
+def weighted_loss(y_true, y_pred):
+    """Binary Crossentropy + Dice loss with weighting"""
+    alpha = 0.3  # Increase weight of positive pixels
+    bce = tf.keras.losses.binary_crossentropy(y_true, y_pred)
+    dice = 1 - (2 * tf.reduce_sum(y_true * y_pred) + 1e-6) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + 1e-6)
+    return alpha * bce + (1 - alpha) * dice  # Weighted combination
